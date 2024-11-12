5f4fd22d07d473242955d5aaf7b525c60beda897
Sanomann <tahamatt1@gmail.com>
2024-11-12 12:33:28 +0100

    Update Program.cs

--- Parkeringsautomaten/Program.cs
+++ Parkeringsautomaten/Program.cs
@@ -6,4 +6,21 @@ using System.Threading.Tasks;
 
 namespace ParkingSystem
 {
-    
+    abstract class vehicle
+    {
+
+        public string regNumber { get; }
+
+        public string Color { get; }
+
+        protected vehicle( string regNumber,string Color)
+        {
+            RegNumber=RegNumber
+            Color=color    
+        }   
+
+    public abstract dubble calculateParkingFee(int parkedSeconds);
+    public abstract dubble spotsNedded { get; }
+}
+    class Car ; vehicle
+{
